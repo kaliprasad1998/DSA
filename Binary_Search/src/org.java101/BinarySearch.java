c597f533c229d42c26d65680f5c2b83708502ea0
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-01-12 19:33:51 +0530

    add pro tip in binary search

--- Binary_Search/src/org.java101/BinarySearch.java
+++ Binary_Search/src/org.java101/BinarySearch.java
@@ -17,6 +17,7 @@ public class BinarySearch {
             // if start and end are very big then start + end may result a no greater than range of int
 
             int mid = start + (end - start) / 2;
+            // Always start with scenario where target < mid
             if (target < arr[mid]) {
                 end = mid - 1;
             } else if (target > arr[mid]) {
