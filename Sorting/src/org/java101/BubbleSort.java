70adbd5f890d4c921c894d3f7a6b624c6f6b2bd5
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-02-02 12:29:11 +0530

    clean up

--- Sorting/src/org/java101/BubbleSort.java
+++ Sorting/src/org/java101/BubbleSort.java
@@ -6,7 +6,6 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 4, 2};
         bubbleSort(nums);
-        System.out.println(nums.length);
         System.out.println(Arrays.toString(nums));
     }
 
