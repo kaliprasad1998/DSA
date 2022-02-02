f73dfda7e179034cc58a47cee51660359ce79914
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-02-02 12:28:02 +0530

    add bubble sort algorithm

---
+++ Sorting/src/org/java101/BubbleSort.java
@@ -0,0 +1,29 @@
+package org.java101;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5};
+        bubbleSort(nums);
+        System.out.println(nums.length);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
