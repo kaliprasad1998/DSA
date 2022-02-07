25a08e48966b655a39080e707e77ed99d9c483e9
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-02-07 21:43:10 +0530

    add selection sort

--- Sorting/src/org/java101/SelectionSort.java
+++ Sorting/src/org/java101/SelectionSort.java
@@ -3,35 +3,34 @@ package org.java101;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] nums = {3, 4, -5, 0, 1, 2};
-        selectionSort(nums);
-        System.out.println(Arrays.toString(nums));
-    }
 
-    public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int lastIndex = arr.length - i - 1;
-            int maxElementIndex = findMax(arr, 0, lastIndex);
-            if (lastIndex != maxElementIndex) {
-                swap(arr, maxElementIndex, lastIndex);
-            }
-        }
-    }
+  public static void main(String[] args) {
+    int[] nums = {3,5,4,1,2,0,-53};
+    selectionSort(nums);
+    System.out.println(Arrays.toString(nums));
+  }
 
-    public static int findMax(int[] arr, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[i] > arr[max]) {
-                max = i;
-            }
-        }
-        return max;
+  static void selectionSort(int[] arr){
+    for (int i = arr.length - 1; i > 0 ; i--) {
+      int maxIndex = findMax(arr,i);
+      swap(arr,maxIndex,i);
     }
+  }
 
-    public static void swap(int[] arr, int firstIndex, int secondIndex) {
-        int temp = arr[firstIndex];
-        arr[firstIndex] = arr[secondIndex];
-        arr[secondIndex] = temp;
+  static int findMax(int[] arr, int end){
+    int max = 0;
+    for (int i = 1; i <= end ; i++) {
+      if(arr[i] > arr[max]){
+        max = i;
+      }
     }
+    return max;
+  }
+
+  static void swap(int[] arr, int i1, int i2){
+    int temp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
+  }
+
 }
