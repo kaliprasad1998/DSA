28beaa116e222a5af08e0f62de49235398eba017
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-06 14:01:04 +0530

    add merge sorted array problem

---
+++ Sorting/src/org/java101/MergerSortedArray_Leet.java
@@ -0,0 +1,38 @@
+package org.java101;
+
+import java.util.Arrays;
+
+class MergerSortedArray_Leet {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, nums1.length, nums2, nums2.length);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        insertionSort(nums1);
+    }
+
+    // Use Insertion sort because arrays are partly sorted
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
+}
