95a0e99ae86ff15544103f79645f04291ec0d863
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-08 19:55:29 +0530

    add contains duplicate problem

---
+++ Sorting/src/org/java101/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+package org.java101;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+// https://leetcode.com/problems/contains-duplicate/
+class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1};
+        System.out.println(containsDuplicate1(arr));
+    }
+    public static boolean containsDuplicate1(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        if(set.size() == nums.length){
+            return false;
+        }
+
+        return true;
+    }
+
+    // Faster as per leet code
+    public static boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
