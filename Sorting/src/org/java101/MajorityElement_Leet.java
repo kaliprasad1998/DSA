f73c9f9a3bd05f9d83e3b0fdd74289d64704696e
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-08 19:40:58 +0530

    add alternate approaches

--- Sorting/src/org/java101/MajorityElement_Leet.java
+++ Sorting/src/org/java101/MajorityElement_Leet.java
@@ -1,16 +1,60 @@
 package org.java101;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
 
-
+// https://leetcode.com/problems/majority-element/
 class MajorityElement_Leet {
 
     public static void main(String[] args) {
-        int[] arr = {3,3,4};
-        int out = majorityElement(arr);
+        int[] arr = {4,4,4,5,5,3};
+        int out = majorityElement1(arr);
         System.out.println(out);
+    }
+    // Sorting
+    public static int majorityElement1(int[] nums) {
+        // Arrays.sort(nums);
+        // SelectionSort
+        for (int i = 0; i < nums.length; i++) {
+            int maxIndex = findMax(nums, nums.length - 1 - i);
+            if(nums[nums.length - 1 -i] != nums[maxIndex]) {
+                swap(nums,maxIndex,nums.length - 1 -i);
+            }
+        }
+
+        return nums[nums.length/2];
+    }
+
+    public static void swap(int[] arr, int i1, int i2){
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
+    public static int findMax(int[] arr, int lastIndex){
+        int ret = 0;
+        for (int i = 1; i <= lastIndex ; i++) {
+            if(arr[i] > arr[ret]){
+                ret = i;
+            }
+        }
+        return ret;
+    }
+
+    // Moore voting algorithm
+    // Didn't understand this shit 😒😂
+    public static int majorityElement3(int[] nums) {
+        int count=0, ret = 0;
+        for (int num: nums) {
+            if (count==0)
+                ret = num;
+            if (num!=ret)
+                count--;
+            else
+                count++;
+        }
+        return ret;
     }
 
     public static int majorityElement(int[] nums) {
