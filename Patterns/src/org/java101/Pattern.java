e8e9cb568f401afcda425aa169cd1b8561f0faed
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-13 17:45:12 +0530

    add patterns problem 6

--- Patterns/src/org/java101/Pattern.java
+++ Patterns/src/org/java101/Pattern.java
@@ -6,7 +6,8 @@ public class Pattern {
 //        pattern2(5);
 //        pattern3(5);
 //        pattern4(5);
-        pattern5(5);
+//        pattern5(5);
+        pattern6(5);
     }
 
     public static void pattern1(int n) {
@@ -36,25 +37,39 @@ public class Pattern {
         }
     }
 
-    public static void pattern4 (int n){
-        for (int i = 1 ; i <= n ; i++) {
-            for(int j = 1; j<= i; j++) {
+    public static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
     }
 
-    public static void pattern5 (int n) {
+    public static void pattern5(int n) {
         int count = 0;
-        for (int i = 1 ; i < 2 * n  ; i++) {
-            if(i <= n ) {
+        for (int i = 1; i < 2 * n; i++) {
+            if (i <= n) {
                 count++;
             } else {
                 count--;
             }
             for (int j = 0; j < count; j++) {
-                System.out.print( "* ");
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern6(int n) {
+        for (int i = 1; i <= n; i++) {
+            int noOfSpaces = n - i;
+            for (int j = 0; j < noOfSpaces; j++) {
+                System.out.print(" ");
+            }
+            int noOfStars = i;
+            for (int j = 0; j < noOfStars; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
