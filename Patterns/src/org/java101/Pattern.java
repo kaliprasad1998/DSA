564c546db830a76fa500adc4101e958fdbc3ed4f
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-13 17:03:16 +0530

    add patterns problem 4

--- Patterns/src/org/java101/Pattern.java
+++ Patterns/src/org/java101/Pattern.java
@@ -4,7 +4,8 @@ public class Pattern {
     public static void main(String[] args) {
 //        pattern1(5);
 //        pattern2(5);
-        pattern3(5);
+//        pattern3(5);
+        pattern4(5);
     }
 
     public static void pattern1(int n) {
@@ -34,5 +35,13 @@ public class Pattern {
         }
     }
 
+    public static void pattern4 (int n){
+        for (int i = 1 ; i <= n ; i++) {
+            for(int j = 1; j<= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
