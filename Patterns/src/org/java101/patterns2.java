3efa66267ba91a8b2591d0c6d2a9a158c7cc40a1
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-13 19:08:03 +0530

    add pattern problem 13

--- Patterns/src/org/java101/patterns2.java
+++ Patterns/src/org/java101/patterns2.java
@@ -2,7 +2,33 @@ package org.java101;
 
 public class patterns2 {
     public static void main(String[] args) {
-
+        pattern13(5);
     }
+    
+    public static void pattern13 (int n) {
+        for (int i = 1; i <= n ; i++) {
+            int firstSpaces = i != n ? n - i : 0;
+            for (int j = 0; j < firstSpaces; j++) {
+                System.out.print(" ");
+            }
+            int firstStars = i != n ? 1 : 0;
+            for (int j = 0; j < firstStars; j++) {
+                System.out.print("*");
+            }
+            int secondSpaces = i > 1 && i != n ? 2 * i - 3 : 0;
+            for (int j = 0; j < secondSpaces; j++) {
+                System.out.print(" ");
+            }
+            int secondStars = i < n && i > 1 ? 1 : 0;
+            for (int j = 0; j < secondStars; j++) {
+                System.out.print("*");
+            }
+            int lastStars = i == n ? 2 * n - 1 : 0;
+            for (int j = 0; j < lastStars; j++) {
+                System.out.print("*");
+            }
 
+            System.out.println();
+        }
+    }
 }
