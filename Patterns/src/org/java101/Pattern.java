c9f40524db5a4c81ccdad6297b2f3b888fcdfec5
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-13 16:56:12 +0530

    add patterns problem 2

--- Patterns/src/org/java101/Pattern.java
+++ Patterns/src/org/java101/Pattern.java
@@ -2,12 +2,22 @@ package org.java101;
 
 public class Pattern {
     public static void main(String[] args) {
-        pattern1(5);
+//        pattern1(5);
+        pattern2(5);
     }
 
-    public static void pattern1(int n){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <n; j++) {
+    public static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
