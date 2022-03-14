93455ea73cda155198a1abc805d90e5a9a0c6936
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-14 11:23:23 +0530

    add patterns problem 18

--- Patterns/src/org/java101/patterns2.java
+++ Patterns/src/org/java101/patterns2.java
@@ -6,11 +6,12 @@ public class patterns2 {
 //        pattern14(5);
 //        pattern15(5);
 //        pattern16(5);
-        pattern17(4);
+//        pattern17(4);
+        pattern18(5);
     }
-    
-    public static void pattern13 (int n) {
-        for (int i = 1; i <= n ; i++) {
+
+    public static void pattern13(int n) {
+        for (int i = 1; i <= n; i++) {
             int firstSpaces = i != n ? n - i : 0;
             for (int j = 0; j < firstSpaces; j++) {
                 System.out.print(" ");
@@ -36,8 +37,8 @@ public class patterns2 {
         }
     }
 
-    public static void pattern14 (int n) {
-        for (int i = 1; i <= n ; i++) {
+    public static void pattern14(int n) {
+        for (int i = 1; i <= n; i++) {
             int firstSpaces = i > 1 ? i - 1 : 0;
             for (int j = 0; j < firstSpaces; j++) {
                 System.out.print(" ");
@@ -63,7 +64,7 @@ public class patterns2 {
         }
     }
 
-    public static void pattern15 (int n) {
+    public static void pattern15(int n) {
         int secondSpaces = 0;
         for (int i = 1; i <= 2 * n - 1; i++) {
             int firstSpaces = i <= n ? n - i : i - n;
@@ -71,26 +72,26 @@ public class patterns2 {
                 System.out.print(" ");
             }
             System.out.print("*");
-            if(i <= 5) {
-                secondSpaces = 2 * i - 3 ;
+            if (i <= 5) {
+                secondSpaces = 2 * i - 3;
             } else {
                 secondSpaces = secondSpaces - 2;
             }
-            if(secondSpaces < 0) {
+            if (secondSpaces < 0) {
                 secondSpaces = 0;
             }
             for (int j = 0; j < secondSpaces; j++) {
                 System.out.print(" ");
             }
             int secondStars = i != 2 * n - 1 && i != 1 ? 1 : 0;
-            for (int j = 1; j <= secondStars ; j++) {
+            for (int j = 1; j <= secondStars; j++) {
                 System.out.print("*");
-            } 
+            }
             System.out.println();
         }
     }
 
-    public static void pattern16 (int n) {
+    public static void pattern16(int n) {
 
         for (int i = 0; i < n; i++) {
             int spaces = n - i - 1;
@@ -98,38 +99,56 @@ public class patterns2 {
                 System.out.print(" ");
             }
             for (int j = 0; j < i + 1; j++) {
-                int num = factorial(i)/(factorial(j) * factorial(i - j));
+                int num = factorial(i) / (factorial(j) * factorial(i - j));
                 System.out.print(num + " ");
             }
             System.out.println();
         }
     }
 
-    private static int factorial (int n) {
+    private static int factorial(int n) {
         int product = 1;
-        if(n == 0 || n == 1) {
+        if (n == 0 || n == 1) {
             return 1;
         } else {
-            for (int i = n; i > 1 ; i--) {
+            for (int i = n; i > 1; i--) {
                 product = product * i;
             }
         }
         return product;
     }
 
-    public static void pattern17 (int n) {
-        for (int i = 1; i <= 2 * n - 1 ; i++) {
+    public static void pattern17(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
             int noOfSpaces = i <= 4 ? n - i : i - n;
             for (int j = 0; j < noOfSpaces; j++) {
                 System.out.print(" ");
             }
             int noOfFirstNums = i <= 4 ? i : 2 * n - i;
-            for (int j = noOfFirstNums; j >= 1  ; j--) {
+            for (int j = noOfFirstNums; j >= 1; j--) {
                 System.out.print(j);
             }
             int secondNums = i <= 4 ? i + 1 : 2 * n - i + 1;
-            for (int j = 2; j < secondNums  ; j++) {
+            for (int j = 2; j < secondNums; j++) {
                 System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern18(int n) {
+
+        for (int i = 1; i <= 2 * n; i++) {
+            int stars = i <= 5 ? n - i + 1 : i - n;
+            int spaces = i <= 5 ? 2 * (i - 1) : 2 * (2 * n - i);
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
