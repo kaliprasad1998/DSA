7fb22b5c74886c08c2e3d8a206cde49bc4af59fd
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-14 18:59:35 +0530

    add patterns problem 25

--- Patterns/src/org/java101/patterns3.java
+++ Patterns/src/org/java101/patterns3.java
@@ -2,6 +2,28 @@ package org.java101;
 
 public class patterns3 {
     public static void main(String[] args) {
-
+        pattern25(6);
+    }
+    
+    public static void pattern25(int n) {
+        for (int i = 1; i <= n ; i++) {
+            int noOfFirstSpaces = n - i;
+            for (int j = 0; j < noOfFirstSpaces; j++) {
+                System.out.print(" ");
+            }
+            int noOfFirstStars = i != 1 && i !=n ? 1 : n;
+            for (int j = 0; j < noOfFirstStars; j++) {
+                System.out.print("*");
+            }
+            int noOfSecondSpaces =  i != 1 && i !=n ? n - 2 : 0;
+            for (int j = 0; j < noOfSecondSpaces; j++) {
+                System.out.print(" ");
+            }
+            int noOfSecondStars = i != 1 && i !=n ? 1 : 0;
+            for (int j = 0; j < noOfSecondStars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
