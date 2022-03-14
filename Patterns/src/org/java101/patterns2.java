01738110c012e68b45b76453ba71b1d3680ff283
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-14 10:59:24 +0530

    add patterns problem 16

--- Patterns/src/org/java101/patterns2.java
+++ Patterns/src/org/java101/patterns2.java
@@ -4,7 +4,8 @@ public class patterns2 {
     public static void main(String[] args) {
 //        pattern13(5);
 //        pattern14(5);
-        pattern15(5);
+//        pattern15(5);
+        pattern16(5);
     }
     
     public static void pattern13 (int n) {
@@ -86,5 +87,32 @@ public class patterns2 {
             } 
             System.out.println();
         }
+    }
+
+    public static void pattern16 (int n) {
+
+        for (int i = 0; i < n; i++) {
+            int spaces = n - i - 1;
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                int num = factorial(i)/(factorial(j) * factorial(i - j));
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int factorial (int n) {
+        int product = 1;
+        if(n == 0 || n == 1) {
+            return 1;
+        } else {
+            for (int i = n; i > 1 ; i--) {
+                product = product * i;
+            }
+        }
+        return product;
     }
 }
