34e7e4aa0bda6cb0be39d6fd50c6c4b967d211f6
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-18 21:11:31 +0530

    add fibonacci problem

---
+++ Recursion/src/org/java101/Fibonacci.java
@@ -0,0 +1,15 @@
+package org.java101;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int out = getFibonacci(4);
+        System.out.println(out);
+    }
+
+    public static int getFibonacci(int n) {
+        if(n < 2) {
+            return n;
+        }
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+}
