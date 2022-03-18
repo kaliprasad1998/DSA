1267e465287925345b0ffe6f5b7a8f3b0b1d27d6
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-03-18 21:13:30 +0530

    clean up

--- Recursion/src/org/java101/Fibonacci.java
+++ Recursion/src/org/java101/Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
     }
 
     public static int getFibonacci(int n) {
-        if(n < 2) {
+        if (n > 0 && n < 2) {
             return n;
         }
         return getFibonacci(n - 1) + getFibonacci(n - 2);
